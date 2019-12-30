2a658368dd67457819563319f228beb899877450
sultanowskii <dsr431@yandex.ru>
2019-12-30 14:57:57 +0300

    Added abstract class "AnimatedSprite" and primitive Main-class
    Signed-off-by: sultanowskii <dsr431@yandex.ru>

---
+++ Main.py
@@ -0,0 +1,30 @@
+import pygame
+import AnimatedSprite
+
+SIZE = X, Y = 800, 600
+FPS = 60
+BLACK = pygame.Color("black")
+
+pygame.init()
+screen = pygame.display.set_mode((X, Y))
+clock = pygame.time.Clock()
+running = True
+screen.fill(BLACK)
+#   make a groups, additional functions
+
+
+def exit_game():    # quiting pygame
+    running = False
+    pygame.quit()
+
+
+while running:
+    screen.fill((255, 255, 255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game()
+        #   all events checker
+    pass
+    #  here are updates of sprite groups
+    pygame.display.flip()
+    clock.tick(FPS)
