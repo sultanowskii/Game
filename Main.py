64f10b55027538c9c5ea5e2bfe1e4c7550e0c6db
sultanowskii <dsr431@yandex.ru>
2020-01-05 21:23:21 +0300

    Modified Main, added some functions
    Signed-off-by: sultanowskii <dsr431@yandex.ru>

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import pygame
 from AnimatedSprite import AnimatedSprite
 from Camera import Camera
 from Tile import Tile
-
+import os
 
 SIZE = X, Y = 800, 600
 FPS = 60
@@ -14,6 +14,45 @@ clock = pygame.time.Clock()
 running = True
 screen.fill(BLACK)
 #   make a groups, additional functions
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def start_screen(): #   Это висит как пример, потом переделай
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Ты умеешь ходить,",
+                  "И все"]
+    background = pygame.transform.scale(load_image('fon.jpg'), (X, Y))
+    screen.blit(background, (0, 0))
+    font = pygame.font.Font(None, 60)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
 
 
 def exit_game():    # quiting pygame
