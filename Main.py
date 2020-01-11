539702f8a58e9191193151a2597d4a0b5891670f
sultanowskii <dsr431@yandex.ru>
2020-01-11 14:09:46 +0300

    Improved readability of the code, made a Pause screen
    Signed-off-by: sultanowskii <dsr431@yandex.ru>

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ import os
 SIZE = X, Y = 800, 600
 FPS = 60
 BLACK = pygame.Color("black")
+WHITE = pygame.Color("white")
 
 pygame.init()
 
@@ -25,8 +26,9 @@ def load_image(name, colorkey=None):
     return image
 
 
+pause_screen_cnecker = False
 music_checker = True
-main_music = 'data/music.mp3' # Jason Garner & Vince de Vera – Creepy Forest (Vinyl) (Don t Starve OST)
+main_music = 'data/music.mp3'  # Jason Garner & Vince de Vera – Creepy Forest (Vinyl) (Don t Starve OST)
 screen = pygame.display.set_mode((X, Y))
 clock = pygame.time.Clock()
 running = True
@@ -48,8 +50,8 @@ player_sprite = load_image('player_sprite.png')
 enemy_sprite = load_image('player_sprite.png')
 
 
-def start_screen():  # Это висит как пример, потом переделай
-    background = pygame.transform.scale(load_image('background.jpg'), (X, Y))
+def start_screen():
+    background = pygame.transform.scale(load_image('background_start.jpg'), (X, Y))
     screen.blit(background, (0, 0))
     while True:
         for event in pygame.event.get():
@@ -57,6 +59,20 @@ def start_screen():  # Это висит как пример, потом пер�
                 exit_game()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def pause_screen():
+    background = pygame.transform.scale(load_image('background_pause.jpg'), (X, Y))
+    screen.blit(background, (0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -69,10 +85,14 @@ def generate_level(level):
                 Tile('empty', x, y, grounds_group, all_sprites, tile_images)
             elif level[y][x] == '#':
                 Tile('wall', x, y, walls_group, all_sprites, tile_images)
-                Border("left", x * 50, y * 50, x * 50, (y + 1) * 50, left_walls_group)  # making invisible borders to checking collide
-                Border("right", (x + 1) * 50, y * 50, (x + 1) * 50, (y + 1) * 50, right_walls_group) # I'm really not sure that it doesnt
-                Border("up", x * 50, y * 50, (x + 1) * 50, y * 50, up_walls_group) # need have at least 1 width. If it does, just update
-                Border("down", x * 50, (y + 1) * 50, (x + 1) * 50, (y + 1) * 50, down_walls_group) # it very early to make it covered with others titles and sprites.
+                Border("left", x * 50, y * 50, x * 50, (y + 1) * 50,
+                       left_walls_group)  # making invisible borders to checking collide
+                Border("right", (x + 1) * 50, y * 50, (x + 1) * 50, (y + 1) * 50,
+                       right_walls_group)  # I'm really not sure that it doesnt
+                Border("up", x * 50, y * 50, (x + 1) * 50, y * 50,
+                       up_walls_group)  # need have at least 1 width. If it does, just update
+                Border("down", x * 50, (y + 1) * 50, (x + 1) * 50, (y + 1) * 50,
+                       down_walls_group)  # it very early to make it covered with others titles and sprites.
             elif level[y][x] == '@':
                 Tile('empty', x, y, grounds_group, all_sprites, tile_images)
                 new_player = Player(player_sprite, ENTER_HERE_COLUMNS_AND_ROWS, x * 50, y * 50, player_group)
@@ -85,6 +105,7 @@ def exit_game():  # quiting pygame
     running = False
     pygame.quit()
 
+
 start_screen()
 camera = Camera(X, Y)
 pygame.mixer.music.load(main_music)
@@ -95,7 +116,8 @@ while running:
             exit_game()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            if not pygame.sprite.spritecollideany(player, right_walls_group): # checking if our player collide with some wall from any side
+            if not pygame.sprite.spritecollideany(player,
+                                                  right_walls_group):  # checking if our player collide with some wall from any side
                 player.move_right()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             if not pygame.sprite.spritecollideany(player, left_walls_group):
@@ -106,8 +128,12 @@ while running:
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             if not pygame.sprite.spritecollideany(player, down_walls_group):
                 player.move_down()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pause_screen()
+                break
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.pos == [0, 0]: # buttons
+            if event.pos == [0, 0]:  # buttons
                 if music_checker:
                     pygame.mixer.music.pause()
                     music_checker = False
