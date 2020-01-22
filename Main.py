d8d34c3233611fbadedf298bffab973f494e4267
sultanowskii <dsr431@yandex.ru>
2020-01-22 10:57:44 +0300

    Fixed moving
    Signed-off-by: sultanowskii <dsr431@yandex.ru>

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from Tile import Tile
 from Enemy import Enemy
 from Player import Player
 from Border import Border
+# from Button import Button
 import os
 import sys
 
@@ -42,6 +43,13 @@ lx = -1
 ly = -1
 ground_width = 0
 ground_height = 0
+going_up = False
+going_down = False
+going_right = False
+going_left = False
+music_off_button_sprite = load_image("music_off.png")
+music_on_button_sprite = load_image("music_on.png")
+# music_button = MusicButton(700, 20, music_on_button_sprite, music_off_button_sprite, buttons_group)
 
 buttons_group = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
@@ -110,17 +118,17 @@ def generate_level(level):
                 Tile('ground', x, y, grounds_group, all_sprites, tile_images, tile_width, tile_height, lx, ly)
             elif level[y][x] == '#':
                 Tile('wall', x, y, walls_group, all_sprites, tile_images, tile_width, tile_height, lx, ly)
-                Border("left", x * tile_width + lx, y * tile_height + ly, x * tile_width + lx,
-                       (y + 1) * tile_height + ly - 1,
+                Border("left", x * tile_width + lx, y * tile_height + ly + 1, x * tile_width + lx,
+                       (y + 1) * tile_height + ly - 2,
                        left_walls_group)  # making invisible borders to checking collide
-                Border("right", (x + 1) * tile_width + lx, y * tile_height + ly, (x + 1) * tile_width + lx,
-                       (y + 1) * tile_height + ly - 1,
+                Border("right", (x + 1) * tile_width + lx, y * tile_height + ly + 1, (x + 1) * tile_width + lx,
+                       (y + 1) * tile_height + ly - 2,
                        right_walls_group)  # I'm really not sure that it doesnt
-                Border("up", x * tile_width + lx, y * tile_height + ly, (x + 1) * tile_width + lx - 1,
+                Border("up", x * tile_width + lx + 1, y * tile_height + ly, (x + 1) * tile_width + lx - 2,
                        y * tile_height + ly,
                        up_walls_group)  # need have at least 1 width. If it does, just update
-                Border("down", x * tile_width + lx, (y + 1) * tile_height + ly, (x + 1) * tile_width + lx - 1,
-                       (y + 1) * tile_height + ly,
+                Border("down", x * tile_width + lx + 1, (y + 1) * tile_height + ly - 1, (x + 1) * tile_width + lx - 2,
+                       (y + 1) * tile_height + ly - 1,
                        down_walls_group)  # it very early to make it covered with others titles and sprites.
             elif level[y][x] == '@':
                 Tile('ground', x, y, grounds_group, all_sprites, tile_images, tile_width, tile_height, lx, ly)
@@ -142,11 +150,8 @@ def terminate():
 
 
 pygame.mixer.music.load(main_music)
-pygame.mixer.music.play()
+pygame.mixer.music.play(1000000)
 player, level_x, level_y, level_map = generate_level(load_level("level.txt"))
-# for enemy in enemies_group:
-#     enemy.n = ground_height
-#     enemy.m = ground_width
 start_screen()
 while running:
     screen.fill(BLACK)
@@ -155,32 +160,64 @@ while running:
             terminate()
             break
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            if not pygame.sprite.spritecollideany(player,
-                                                  left_walls_group):
-                # checking if our player collide with some wall from any side
-                player.move_right(1)
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            if not pygame.sprite.spritecollideany(player, right_walls_group):
-                player.move_left(1)
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            if not pygame.sprite.spritecollideany(player, down_walls_group):
-                player.move_up(1)
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            if not pygame.sprite.spritecollideany(player, up_walls_group):
-                player.move_down(1)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pause_screen()
                 break
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.pos == [0, 0]:  # buttons
-                if music_checker:
-                    pygame.mixer.music.pause()
-                    music_checker = False
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                if not pygame.sprite.spritecollideany(player,
+                                                      left_walls_group):
+                    # checking if our player collide with some wall from any side
+                    going_right = True
                 else:
-                    pygame.mixer.music.unpause()
-                    music_checker = True
+                    going_right = False
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                if not pygame.sprite.spritecollideany(player, right_walls_group):
+                    going_left = True
+                else:
+                    going_left = False
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                if not pygame.sprite.spritecollideany(player, down_walls_group):
+                    going_up = True
+                else:
+                    going_up = False
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                if not pygame.sprite.spritecollideany(player, up_walls_group):
+                    going_down = True
+                else:
+                    going_down = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                # Turning off moving if we stopped pressing key
+                going_right = False
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                going_left = False
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                going_up = False
+            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                going_down = False
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        #     if event.pos == [0, 0]:  # buttons
+        #         if music_buton.isMouseOn(event.pos):
+        #             music_button.switch()
+        #             if music_checker:
+        #                 music_checker = False
+        #                 pygame.mixer.stop()
+        #             else:
+        #                 music_checker = True
+        #                 pygame.mixer.unpause()
+
+    if going_down:
+        #   if people can go and he wants go (key pressed), every iteration we move him to his speed
+        player.move_down(1)
+    if going_up:
+        player.move_up(1)
+    if going_right:
+        player.move_right(1)
+    if going_left:
+        player.move_left(1)
+
+    #   if people collided with wall, we make him go back (we must do this with player's speed)
     if pygame.sprite.spritecollideany(player, left_walls_group):
         player.move_left(1)
     if pygame.sprite.spritecollideany(player, right_walls_group):
@@ -189,6 +226,7 @@ while running:
         player.move_up(1)
     if pygame.sprite.spritecollideany(player, down_walls_group):
         player.move_down(1)
+
     player_group.update()
     enemies_group.update()
     # buttons_group.update()
