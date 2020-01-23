53e8843777ce129c1336f80392d8259f0685167c
sultanowskii <dsr431@yandex.ru>
2020-01-23 16:33:35 +0300

    Added texts, chnges main
    Signed-off-by: sultanowskii <dsr431@yandex.ru>

--- Main.py
+++ Main.py
@@ -44,7 +44,6 @@ going_up = False
 going_down = False
 going_right = False
 going_left = False
-pause_screen_cnecker = False
 running = True
 
 lamp_up_sprite = load_image("lamp_up.png", (235, 255, 255))
@@ -72,7 +71,6 @@ main_music = 'data/music.mp3'  # Jason Garner & Vince de Vera – Creepy Forest 
 
 lamps_group = pygame.sprite.Group()
 buttons_group = pygame.sprite.Group()
-all_sprites = pygame.sprite.Group()
 grounds_group = pygame.sprite.Group()
 walls_group = pygame.sprite.Group()
 left_walls_group = pygame.sprite.Group()
@@ -89,6 +87,39 @@ player = None
 
 def start_screen():
     x = 1
+    y = 1
+    print("\033[33mПривет! Нажав в игровом окне любую кнопку, то вы загрузите уровни,")
+    print("созданные \033[35mразработичками.\033[0m")
+    print("\033[33mЕсли же вы хотите сыграть в \033[32mсвой уровень\033[33m (инструкция по ее созданию")
+    print("\033[33mесть в папке с игрой), то тогда нажмите с открытым окном клавишу '"
+          "'5'',\nпотом вставьте сюда название своего уровня, ", end="")
+    print("если хотите сыграть на своем:\n")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+                return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_5:
+                    level = input()
+                    return level
+                return
+        background = pygame.transform.scale(load_image(f'bck_frames\\bck_start{x}.png'), (X, Y))
+        text = pygame.transform.scale(load_image(f'continue_text\\press_text{y // 5 + 1}.png', (235, 255, 255)), (810, 54))
+        screen.blit(background, (0, 0))
+        screen.blit(text, (-3, 530))
+        x = (x + 1) % 40 + 1
+        y = (y + 1) % 40 + 1
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def pause_screen():
+    x = 1
+    y = 1
+    pause_table = pygame.transform.scale(load_image("pause_text.png", (235, 255, 255)), (720, 171))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -96,24 +127,14 @@ def start_screen():
                 return
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 return
-        background = pygame.transform.scale(load_image(f'bck_frames\\bck_start{x % 40 + 1}.png'), (X, Y))
+        background = pygame.transform.scale(load_image(f'bck_frames\\bck_start{x}.png'), (X, Y))
+        text = pygame.transform.scale(load_image(f'continue_text\\press_text{y // 5 + 1}.png', (235, 255, 255)),
+                                      (810, 54))
         screen.blit(background, (0, 0))
-        x += 1
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-def pause_screen():
-    background = pygame.transform.scale(load_image('background_pause.jpg'), (X, Y))
-    screen.blit(background, (0, 0))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-                return
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return
+        screen.blit(text, (-3, 530))
+        screen.blit(pause_table, (45, 5))
+        x = (x + 1) % 40 + 1
+        y = (y + 1) % 40 + 1
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -130,13 +151,14 @@ def generate_level(level):
     new_player, x, y = None, None, None
     ly = (Y - len(level) * tile_height) // 2
     lx = (X - len(level[0]) * tile_width) // 2
+    ens = 0
     id_cntr = 0
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
-                Tile('ground', x, y, grounds_group, all_sprites, tile_images, tile_width, tile_height, lx, ly)
+                Tile('ground', x, y, grounds_group, tile_images, tile_width, tile_height, lx, ly)
             elif level[y][x] == '#':
-                Tile('wall', x, y, walls_group, all_sprites, tile_images, tile_width, tile_height, lx, ly)
+                Tile('wall', x, y, walls_group, tile_images, tile_width, tile_height, lx, ly)
                 Border("left", x * tile_width + lx, y * tile_height + ly + 1, x * tile_width + lx,
                        (y + 1) * tile_height + ly - 2,
                        left_walls_group)  # making invisible borders to checking collide
@@ -150,14 +172,15 @@ def generate_level(level):
                        (y + 1) * tile_height + ly - 1,
                        down_walls_group)  # it very early to make it covered with others titles and sprites.
             elif level[y][x] == '@':
-                Tile('ground', x, y, grounds_group, all_sprites, tile_images, tile_width, tile_height, lx, ly)
+                Tile('ground', x, y, grounds_group, tile_images, tile_width, tile_height, lx, ly)
                 new_player = Player(player_sprite, pl_up_sprite, pl_down_sprite, pl_right_sprite, pl_left_sprite, 4, 1,
                                     x * tile_width + (tile_width - charecter_width) / 2 + lx,
                                     y * tile_height + (tile_height - charecter_height) / 2 + ly, player_group)
             elif level[y][x] == '!':
-                Tile('ground', x, y, grounds_group, all_sprites, tile_images, tile_width, tile_height, lx, ly)
+                Tile('ground', x, y, grounds_group, tile_images, tile_width, tile_height, lx, ly)
                 a = x * tile_width + (tile_width - charecter_width) // 2 + lx
                 b = y * tile_height + (tile_height - charecter_height) // 2 + ly
+                ens += 1
                 curr_enemy = Enemy(enemy_sprite, en_up_sprite, en_down_sprite, en_right_sprite, en_left_sprite, 4, 1, a, b, enemies_group, level,
                       (a - lx) // tile_width, (b - ly) // tile_height, id_cntr)
                 curr_lamp = Lamp(curr_enemy.rect.x, curr_enemy.rect.y, lamp_up_sprite, lamp_down_sprite, lamp_right_sprite, lamp_left_sprite, id_cntr, lamps_group)
@@ -167,7 +190,7 @@ def generate_level(level):
     Border("left", lx + (x + 1) * tile_width, ly, lx + (x + 1) * tile_width, (y + 1) * tile_height + ly, left_walls_group)
     Border("down", lx, ly, lx + (x + 1) * tile_width, ly, down_walls_group)
     Border("up", lx, ly + (y + 1) * tile_height, lx + (x + 1) * tile_width, (y + 1) * tile_height + ly, up_walls_group)
-    return new_player, x, y, level
+    return new_player, x, y, level, ens
 
 
 def terminate():
@@ -177,106 +200,138 @@ def terminate():
 
 pygame.mixer.music.load(main_music)
 pygame.mixer.music.play(1000000)
-player, level_x, level_y, level_map = generate_level(load_level("level.txt"))
-start_screen()
-while running:
-    screen.fill(BLACK)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            terminate()
-            break
-        keys = pygame.key.get_pressed()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pause_screen()
+
+
+def clear_groups():
+    lamps_group.empty()
+    grounds_group.empty()
+    walls_group.empty()
+    left_walls_group.empty()
+    right_walls_group.empty()
+    up_walls_group.empty()
+    down_walls_group.empty()
+    player_group.empty()
+    enemies_group.empty()
+
+
+def main(al_cntr):
+    going_up = False
+    going_down = False
+    going_right = False
+    going_left = False
+    alive_cntr = al_cntr
+    while running and alive_cntr > 0:
+        screen.fill(BLACK)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
                 break
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                if not pygame.sprite.spritecollideany(player,
-                                                      left_walls_group):
-                    # checking if our player collide with some wall from any side
-                    going_right = True
-                else:
+            keys = pygame.key.get_pressed()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pause_screen()
+                    break
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    if not pygame.sprite.spritecollideany(player,
+                                                          left_walls_group):
+                        # checking if our player collide with some wall from any side
+                        going_right = True
+                    else:
+                        going_right = False
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    if not pygame.sprite.spritecollideany(player, right_walls_group):
+                        going_left = True
+                    else:
+                        going_left = False
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    if not pygame.sprite.spritecollideany(player, down_walls_group):
+                        going_up = True
+                    else:
+                        going_up = False
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    if not pygame.sprite.spritecollideany(player, up_walls_group):
+                        going_down = True
+                    else:
+                        going_down = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    # Turning off moving if we stopped pressing key
                     going_right = False
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                if not pygame.sprite.spritecollideany(player, right_walls_group):
-                    going_left = True
-                else:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                     going_left = False
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                if not pygame.sprite.spritecollideany(player, down_walls_group):
-                    going_up = True
-                else:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
                     going_up = False
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                if not pygame.sprite.spritecollideany(player, up_walls_group):
-                    going_down = True
-                else:
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     going_down = False
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                # Turning off moving if we stopped pressing key
-                going_right = False
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                going_left = False
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                going_up = False
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                going_down = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if music_button.isMouseOn(event.pos):
-                if music_button.turned:
-                    pygame.mixer.music.pause()
-                else:
-                    pygame.mixer.music.unpause()
-                music_button.switch()
-            if pause_button.isMouseOn(event.pos):
-                pause_screen()
-                break
-    went_anywhere = False
-    if going_down:
-        #   if people can go and he wants go (key pressed), every iteration we move him to his speed
-        went_anywhere = True
-        player.move_down(2)
-    if going_up:
-        player.move_up(2)
-        went_anywhere = True
-    if going_right:
-        player.move_right(2)
-        went_anywhere = True
-    if going_left:
-        player.move_left(2)
-        went_anywhere = True
-    if not went_anywhere:
-        player.stay_on()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if music_button.isMouseOn(event.pos):
+                    if music_button.turned:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+                    music_button.switch()
+                if pause_button.isMouseOn(event.pos):
+                    pause_screen()
+                    break
+        went_anywhere = False
+        if going_down:
+            #   if people can go and he wants go (key pressed), every iteration we move him to his speed
+            went_anywhere = True
+            player.move_down(2)
+        if going_up:
+            player.move_up(2)
+            went_anywhere = True
+        if going_right:
+            player.move_right(2)
+            went_anywhere = True
+        if going_left:
+            player.move_left(2)
+            went_anywhere = True
+        if not went_anywhere:
+            player.stay_on()
 
-    #   if people collided with wall, we make him go back (we must do this with player's speed)
-    if pygame.sprite.spritecollideany(player, left_walls_group):
-        player.move_left(2)
-    if pygame.sprite.spritecollideany(player, right_walls_group):
-        player.move_right(2)
-    if pygame.sprite.spritecollideany(player, up_walls_group):
-        player.move_up(2)
-    if pygame.sprite.spritecollideany(player, down_walls_group):
-        player.move_down(2)
+        #   if people collided with wall, we make him go back (we must do this with player's speed)
+        if pygame.sprite.spritecollideany(player, left_walls_group):
+            player.move_left(2)
+        if pygame.sprite.spritecollideany(player, right_walls_group):
+            player.move_right(2)
+        if pygame.sprite.spritecollideany(player, up_walls_group):
+            player.move_up(2)
+        if pygame.sprite.spritecollideany(player, down_walls_group):
+            player.move_down(2)
 
-    for enemy in enemies_group:
-        if pygame.sprite.collide_rect(player, enemy):
-            enemy.kill()
+        for enemy in enemies_group:
+            if pygame.sprite.collide_rect(player, enemy) and not enemy.dead:
+                alive_cntr -= 1
+                enemy.kill()
 
-    player_group.update()
-    enemies_group.update()
-    buttons_group.update()
-    right_walls_group.draw(screen)
-    left_walls_group.draw(screen)
-    up_walls_group.draw(screen)
-    down_walls_group.draw(screen)
-    grounds_group.draw(screen)
-    lamps_group.draw(screen)
-    enemies_group.draw(screen)
-    player_group.draw(screen)
-    walls_group.draw(screen)
-    buttons_group.draw(screen)
-    pygame.display.flip()
-    clock.tick(FPS)
+        player_group.update()
+        enemies_group.update()
+        buttons_group.update()
+        right_walls_group.draw(screen)
+        left_walls_group.draw(screen)
+        up_walls_group.draw(screen)
+        down_walls_group.draw(screen)
+        grounds_group.draw(screen)
+        lamps_group.draw(screen)
+        enemies_group.draw(screen)
+        player_group.draw(screen)
+        walls_group.draw(screen)
+        buttons_group.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+curr_level = start_screen()
+if curr_level != None:
+    player, level_x, level_y, level_map, enemies_cntr = generate_level(load_level(f"levels/{curr_level}"))
+    main(enemies_cntr)
+else:
+    for i in range(1, 21):
+        player, level_x, level_y, level_map, enemies_cntr = generate_level(load_level(f"levels/level{i}.txt"))
+        running = True
+        main(enemies_cntr)
+        clear_groups()
+        pause_screen()
 
 pygame.quit()
